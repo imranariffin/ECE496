1cb35b6e08c4d49f611ebc4266c99de2124ca997
lichuanr <richard874410593@gmail.com>
2016-08-16 15:34:12 -0400

    host location marker

--- angular-map.js
+++ angular-map.js
@@ -26,6 +26,44 @@ var cities = [
     lng:-123.116226
   }];
 
+  var hosts = [
+  {
+    host: "Casa Loma",
+    desc: "1",
+    lat:43.6924298, 
+    lng:-79.4413491
+  }, 
+  {
+    host: "Upper Canada College",
+    desc: "2",
+    lat:43.6941412, 
+    lng:-79.3981012
+  },
+  {
+    host: "Spadina Station",
+    desc: "3",
+    lat: 43.6711836, 
+    lng: -79.4022474
+  },
+  {
+    host: "Dundurn Castle",
+    desc: "4",
+    lat: 43.2736872,
+    lng: -79.8657758
+  },
+  {
+    host: "Niagara Falls",
+    desc: "5",
+    lat:43.054098,
+    lng:-79.2281196
+  },
+  {
+    host: "Hillcrest",
+    desc: "6",
+    lat:43.8678357,
+    lng:-79.4245072
+  }];
+
 //Angular App Module and Controller
 var sampleApp = angular.module('mapsApp', []);
 sampleApp.controller('MapCtrl', function ($scope) {
@@ -60,8 +98,26 @@ sampleApp.controller('MapCtrl', function ($scope) {
         
     }  
     
-    for (i = 0; i < cities.length; i++){
+    for (i = 0; i < cities.length; i++) {
         createMarker(cities[i]);
+    }
+
+    /* 
+      loading the Markers on a map for the selected city
+    */
+    var createMarkerForCity = function (info, map){
+      // imran tests infoWindow @ imran-7-infowindow
+      marker = new google.maps.Marker({
+        map: map,
+        position: new google.maps.LatLng(info.lat, info.lng)
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        // infoWindow is defined at "./infoWindow.js"
+        infoWindow = createInfoWindow();
+        infoWindow.open(map, this);
+      });
+
     }
 
     /* 
@@ -71,22 +127,16 @@ sampleApp.controller('MapCtrl', function ($scope) {
     */
     $scope.changeCity = function () {  
       var index = $scope.cityIndex;
-      // var index = document.getElementById("city").value;
-      var cityLocation = [{lat:43.6765211, lng:-79.4354023}, {lat:40.7142700, lng:-74.0059700}, {lat:37.7749300, lng:-122.4194200}, {lat:49.246292, lng:-123.116226}];
+      var cityLocation = cities[index];
+
       map = new google.maps.Map(document.getElementById('map'), {
-        center: cityLocation[index],
+        center: {lat: cityLocation.lat, lng: cityLocation.lng},
         zoom: 8
       });
-      // imran tests infoWindow @ imran-7-infowindow
-      marker = new google.maps.Marker({
-        map: map,
-        position: cityLocation[index]
-      });
-      google.maps.event.addListener(marker, 'click', function() {
-        // infoWindow is defined at "./infoWindow.js"
-        infoWindow = createInfoWindow();
-        infoWindow.open(map, this);
-      });
+
+      for (i = 0; i < hosts.length; i++) {
+        createMarkerForCity(hosts[i], map);
+      }
     }
 
 });
