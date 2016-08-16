3e0151617f9a9d9d38a7368415f6fa3b96483b41
imranariffin <imran.ariffin@mail.utoronto.ca>
2016-08-16 15:24:47 -0400

    move GMapMarker to a new file. fix info-window

--- angular-map.js
+++ angular-map.js
@@ -42,37 +42,6 @@ sampleApp.controller('MapCtrl', function ($scope) {
     $scope.markers = [];
     
     var infoWindow = new google.maps.InfoWindow();
-    
-    /* 
-      A custom constructor for google.maps.Marker.
-      Allows you to assign a type ('hoster' or 'city') to marker.
-      The icon of marker will be assigned according to type. 
-      Icon image is located at ./img
-    */
-    function GMapMarker(type, options) {
-      var image = {
-        // This marker is 20 pixels wide by 32 pixels high.
-        size: new google.maps.Size(30, 32),
-        // The origin for this image is (0, 0).
-        origin: new google.maps.Point(0, 0),
-        // The anchor for this image is the base of the flagpole at (0, 32).
-        anchor: new google.maps.Point(0, 32),
-        // 
-        scaledSize: new google.maps.Size(32, 32)
-      };
-      if (type=="hoster") {
-        image.url = "./img/marker-hoster.png";
-        options.icon = image;
-        return new google.maps.Marker(options);
-      } else if(type=="city") {
-        image.url = "./img/marker-city.png";
-        options.icon = image;
-        return new google.maps.Marker(options);
-      } else {
-        return null;
-      }
-    }
-
     var createMarker = function (info){
         
         var marker = GMapMarker("hoster", {
@@ -115,7 +84,7 @@ sampleApp.controller('MapCtrl', function ($scope) {
       });
       google.maps.event.addListener(marker, 'click', function() {
         // infoWindow is defined at "./infoWindow.js"
-        infoWindow = createInfoWindow(infoWindow);
+        infoWindow = createInfoWindow();
         infoWindow.open(map, this);
       });
     }
